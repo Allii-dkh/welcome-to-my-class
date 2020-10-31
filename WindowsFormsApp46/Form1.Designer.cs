95b4fe65b2a9a66797c32320a4b23243ac8c1588
Ali Dolatkhani <A.Dolatkhani@Hoosh.local>
2020-10-31 11:20:29 +0330

    Add project files.

---
+++ WindowsFormsApp46/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp46
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Black;
+            this.titleLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.titleLabel.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(387, 248);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Welcom To My Class ";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.Enabled = true;
+            this.timerLabel.Interval = 1000;
+            this.timerLabel.Tick += new System.EventHandler(this.timerLabel_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 248);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Timer timerLabel;
+    }
+}
+
---
+++ WindowsFormsApp46/Form1.cs
@@ -0,0 +1,38 @@
+﻿namespace WindowsFormsApp46
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timerLabel_Tick(object sender, System.EventArgs e)
+        {
+            if (titleLabel.Text == "Welcome To My Class!")
+            {
+                titleLabel.Text = "I'm Ali Dolatkhani";
+                titleLabel.ForeColor = System.Drawing.Color.Yellow;
+            }
+            else
+            {
+                titleLabel.Text = "Welcome To My Class!";
+                titleLabel.ForeColor = System.Drawing.Color.White;
+            }
+                
+            
+        }
+
+        private void titleLabel_Click(object sender, System.EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("bye bye");
+            System.Windows.Forms.Application.Exit();
+           
+        }
+    }
+ 
+       
+       
+
+ 
+}
